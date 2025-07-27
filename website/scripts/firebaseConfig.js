7b64e015d1c6fef3e4b9671bede55b47e5b155d4
hjpad <cristian@hjpad.com>
2025-07-27 15:09:15 +1000

    auth

---
+++ website/scripts/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmOXPUL4iGsO3GEFAvkekuQaIk8Prpdcs",
+  authDomain: "html-apps.firebaseapp.com",
+  projectId: "html-apps",
+  storageBucket: "html-apps.firebasestorage.app",
+  messagingSenderId: "990723487482",
+  appId: "1:990723487482:web:48ec2f36de888f29d3bb43",
+};
